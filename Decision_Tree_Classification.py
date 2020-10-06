3e582f275f19f0bb5c79c29727b16608fd7d93d9
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-05 18:21:38 -0600

    Model completed in Spyder

--- Decision_Tree_Classification.py
+++ Decision_Tree_Classification.py
@@ -17,8 +17,8 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-from sklearn. import 
-classifier = ()
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 
 print(classifier.predict(sc.transform([[30, 87000]])))
